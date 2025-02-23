afc8e767b0963a6d796d0c95dbfdc805b3c30a13
Dan4C0D3 <danic@ualberta.ca>
2025-02-23 00:53:57 -0700

    Map

---
+++ 2d_grid_movement-master/scenes/left_collision.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+func _on_body_entered(body: Node2D) -> void:
+	print(body.name)
+	if body.is_in_group("Player"):
+		var pos = body.global_position
+		body.global_position = -pos + 100 * (pos / abs(pos))
