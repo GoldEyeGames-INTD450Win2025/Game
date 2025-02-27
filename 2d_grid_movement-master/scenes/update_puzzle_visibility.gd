e5a614ffd32272fdf72d71e0db680d97dfa4cd1c
Brandon Hoynick <hoynick@ualberta.ca>
2025-02-26 21:16:49 -0700

    integrated puzzle with map stuff

--- 2d_grid_movement-master/scenes/update_puzzle_visibility.gd
+++ 2d_grid_movement-master/scenes/update_puzzle_visibility.gd
@@ -1,8 +1,17 @@
 extends ColorRect
-
+var one_time_activator = false
+@onready var demo_done_label = $CenterContainer/CanvasLayer/PanelContainer2
+signal demo_done_music  # Define signal to later emit
 func _ready():
 	visibility_changed.connect(_on_visibility_changed)
 
 func _on_visibility_changed() -> void:
 	var _canvas: CanvasLayer = $CenterContainer/CanvasLayer
 	_canvas.visible = self.visible
+
+func _process(delta: float) -> void:
+	if (Global.puzzle_solved == true) and (one_time_activator == false):
+		one_time_activator = true
+		demo_done_label.show()
+		demo_done_music.emit()
+		
