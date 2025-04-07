7420b64a2f15936f67e16a26b68a744d8c8ceb83
Brandon Hoynick <hoynick@ualberta.ca>
2025-04-06 19:57:41 -0600

    clean up 6 dialogue addresses

--- 2d_grid_movement-master/assets/audio/music_manager_lvl_3.gd
+++ 2d_grid_movement-master/assets/audio/music_manager_lvl_3.gd
@@ -13,39 +13,18 @@ func _ready():
 			print(track)
 		else:
 			print("not track")
-	
 	# Ensure we have exactly 7 tracks
 	if tracks.size() != 7:
 		push_error("MusicManager requires exactly 7 AudioStreamPlayer2D nodes as children!")
 		return
-	
 	# Initialize track volumes
 	initialize_music_layers()
-	
 	# Play all tracks
 	for track in tracks:
 		track.play()
-	
 	# Initialize starting track set
 	initialize_starting_track_set()
-	
-	## Find the Activator1 (Area2D) and connect its signal
-	#var zone_Activator1_Area2D = get_node("../../Activator1_Area2D")  # Adjust path as needed
-	#if zone_Activator1_Area2D:
-		#zone_Activator1_Area2D.player_near_Activator1_Area2D.connect(_on_player_nearby_Activator1)
-		#print("Connected to music signal: Activator1_Area2D")
-		#
-	## Find the Activator2 (Area2D) and connect its signal
-	#var zone_Activator3_Area2D = get_node("../../Activator3_Area2D")  # Adjust path as needed
-	#if zone_Activator3_Area2D:
-		#zone_Activator3_Area2D.player_near_Activator3_Area2D.connect(_on_player_nearby_Activator3)
-		#print("Connected to music signal: Activator3_Area2D")
-		#
-	## Change music for demo done (puzzle solved)
-	#var sigd_demoDone = get_node("/root/Map_Danic/Activator3_Area2D/ColorRect2")  # Adjust path as needed
-	#if sigd_demoDone:
-		#sigd_demoDone.demo_done_music.connect(_on_demoDone)
-		#print("Connected to music signal: demo done")
+
 
 func set_music_layer(index: int, volume_db: float):
 	if index >= 0 and index < tracks.size():
@@ -71,47 +50,10 @@ func set_music_scene_0():
 	set_music_layer(6, mute)
 
 func set_music_scene_1():
-	set_music_layer(0, full) # Full
+	set_music_layer(0, -6) # Full
 	set_music_layer(1, mute)
 	set_music_layer(2, mute)
 	set_music_layer(3, mute) 
 	set_music_layer(4, mute) 
 	set_music_layer(5, mute)
 	set_music_layer(6, mute)
-
-func set_music_scene_2():
-	set_music_layer(0, mute)
-	set_music_layer(1, full)
-	set_music_layer(2, mute)
-	set_music_layer(3, mute) 
-	set_music_layer(4, full*0.5) 
-	set_music_layer(5, mute)
-	set_music_layer(6, mute)
-
-func set_music_scene_3():
-	set_music_layer(0, mute)
-	set_music_layer(1, mute)
-	set_music_layer(2, full)
-	set_music_layer(3, mute) 
-	set_music_layer(4, mute)
-	set_music_layer(5, full)
-	set_music_layer(6, mute)
-	
-func set_music_scene_4():
-	set_music_layer(0, mute)
-	set_music_layer(1, full)
-	set_music_layer(2, mute)
-	set_music_layer(3, mute) 
-	set_music_layer(4, full*0.5) 
-	set_music_layer(5, full*0.5)
-	set_music_layer(6, full*0.5)
-
-func _on_player_nearby_Activator1():
-	#set_music_scene_4()
-	pass
-
-func _on_player_nearby_Activator3():
-	set_music_scene_2()
-
-func _on_demoDone():
-	set_music_scene_4()
